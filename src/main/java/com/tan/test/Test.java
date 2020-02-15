91e9d6dd183ae74c889bee8914b3a612ec72ffc4
tiandiyihao0 <654969283@qq.com>
2020-02-15 17:17:28 +0800

    ddd

--- src/main/java/com/tan/test/Test.java
+++ src/main/java/com/tan/test/Test.java
@@ -3,9 +3,7 @@ package com.tan.test;
 public class Test {
 	
 	public static void main(String[] args) {
-		System.out.println("test2");
 		System.out.println("test1");
 		System.out.println("hotfix_G");
-		System.out.println("hotfix_G2");
 	}
 }
